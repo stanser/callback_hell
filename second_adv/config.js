e2ab315b8e2fed9dad0b5e258ccbc0d9937f0262
a_studenyak <a.studenyak@kwebbl.com>
2015-01-19 15:51:35 +0200

    conflict fixed

--- second_adv/config.js
+++ second_adv/config.js
@@ -15,11 +15,7 @@ var amqp = {
         } 
     },
     queue: {
-<<<<<<< HEAD
         prefix: 'test_stud_queue',
-=======
-        prefix: 'test_stud_queue_',
->>>>>>> bf3b1ae59856a780287418a2e9467cfaf0880cc0
         options: {
             autoDelete: false
         }
@@ -43,26 +39,19 @@ var timeZoneOffset = {
     CEST: 2,
     CET: 1
 };
-<<<<<<< HEAD
 
 var path = {
     models: 'src/Models',
     controllers: 'src/Controllers',
     connector: 'src'
 };
-=======
->>>>>>> bf3b1ae59856a780287418a2e9467cfaf0880cc0
     
 var config = {
     defaultConsumer: defaultConsumer,
     amqp: amqp,
     db: db,
-<<<<<<< HEAD
     timeZoneOffset: timeZoneOffset,
     path: path
-=======
-    timeZoneOffset: timeZoneOffset
->>>>>>> bf3b1ae59856a780287418a2e9467cfaf0880cc0
 }
 
 module.exports = config;
