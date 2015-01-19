fec2f7175f5cacb9cec9ab722918389ef333151c
a_studenyak <a.studenyak@kwebbl.com>
2015-01-19 15:53:42 +0200

    unuseful files deleted

--- second_adv/cons_start.js
+++
@@ -1,7 +0,0 @@
-var ConsumerSrc = require('./src/Consumer.js');
-var Promise = require('bluebird');
-
-var cons_start = new ConsumerSrc.Consumer('start');
-console.log('Promise paradise comes true...');
-
-//cons_start.receiveMessage('start');
--- second_adv/cons_stop.js
+++
@@ -1,3 +0,0 @@
- var ConsumerSrc = require ('./src/Consumer.js');
-var cons_stop = new ConsumerSrc.Consumer('stop');
-cons_stop.receiveMessage();
